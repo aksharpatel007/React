28547e377ddca1fd8b81da14fab3fb20bf291079
Akshar Patel <aksharp1218@gmail.com>
2026-03-03 13:00:10 +0530

    Wrap main content in a fragment for better structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
 
   return (
-    <h1>Hello world with vite </h1>
+    <>
+      <h1>Hello world with vite </h1>
+      <h2>this is my first react app with vite</h2>
+    </>
   )
 }
 
